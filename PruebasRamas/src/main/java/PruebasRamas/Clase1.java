5edd822e21ea5df47182e08404433470e60f9a4c
GorkaCarnero <jon.gonzalezbals@elorritea-errekamari.com>
2020-02-19 11:46:34 +0100

    clase1

--- PruebasRamas/src/main/java/PruebasRamas/Clase1.java
+++ PruebasRamas/src/main/java/PruebasRamas/Clase1.java
@@ -2,8 +2,15 @@ package PruebasRamas;
 
 public class Clase1 {
 	
-	public int cuadrado(int x) {
+	public int cuadrado(int x) 
+	{
 		return x+x;
+	}
+	
+	public int triple(int x) 
+	{
+		return x*3;
+		
 	}
 
 }
--- PruebasRamas/src/test/java/PruebasRamas/testClase1.java
+++ PruebasRamas/src/test/java/PruebasRamas/testClase1.java
@@ -8,10 +8,18 @@ public class testClase1 {
 	private Clase1 clase1 = new Clase1();
 	
 	@Test
-	public void testCuadrado() {
+	public void testCuadrado() 
+	{
 		int resultado = clase1.cuadrado(2);
 		int resultadoEsperado = 4;
 		assertEquals(resultado,resultadoEsperado);
 	}
-
+	
+	@Test
+	public void testTriple()
+	{
+		int resultado = clase1.triple(2);
+		int resultadoEsperado = 6;
+		assertEquals(resultado,resultadoEsperado);
+	}
 }
